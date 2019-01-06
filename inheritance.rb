d782773e70239d57c71f583d851bbe18d350f20b
Barry <bdc1130@yahoo.com>
2019-01-06 11:45:04 -0600

    created inheritance example

---
+++ inheritance.rb
@@ -0,0 +1,46 @@
+class Wheels
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+
+
+end
+
+class Car < Wheels
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Wheels
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+bike1 = Bike.new
+p bike1
+bike1.accelerate
+p bike1
+bike1.ring_bell
+
+car1 = Car.new
+p car1
+car1.accelerate
+p car1
+car1.honk_horn
